6a71671212588e177c9d73a88587152b0faca118
Jean-Michel Cazaux <jeanmichel.cazaux@gmail.com>
2022-05-06 19:28:11 +0200

    WIP: added builder for dependencies

--- src/main/kotlin/brewprocess/dsl/BrewProcessBuilder.kt
+++ src/main/kotlin/brewprocess/dsl/BrewProcessBuilder.kt
@@ -4,8 +4,8 @@ import brewprocess.*
 
 class BrewProcessBuilder {
     var name: String? = null
-    private val tasks: List<Task> = ArrayList()
-    private val dependencies: List<DependencyRepresentation> = ArrayList()
+    var tasks: List<Task> = ArrayList()
+    var dependencies: List<DependencyRepresentation> = ArrayList()
 
     fun build(): BrewProcess {
         val brewProcess = BrewProcess(name ?: throw IllegalArgumentException("process name must be defined"))
@@ -31,8 +31,27 @@ class BrewProcessBuilder {
     }
 }
 
+
 fun brewProcess(brewProcess: BrewProcessBuilder.() -> Unit): BrewProcess =
     BrewProcessBuilder().apply(brewProcess).build()
+
+
+class DependencyBuilder {
+    lateinit var fromTask: String
+    lateinit var toTask: String
+    lateinit var type: DependencyType
+    var delay: Int = 0
+
+    fun build(): DependencyRepresentation {
+        return DependencyRepresentation(
+            fromTask = fromTask,
+            toTask = toTask,
+            type = type,
+            delay = delay
+        )
+    }
+}
+
 
 fun mash(mash: MashBuilder.() -> Unit): Mash = MashBuilder().apply(mash).build()
 
@@ -46,12 +65,21 @@ fun heatWater(heatWater: TaskBuilder.() -> Unit): HeatWater = HeatWaterBuilder()
 
 fun chill(chill: TaskBuilder.() -> Unit): Chill = ChillBuilder().apply(chill).build()
 
+fun dependency(dependency: DependencyBuilder.() -> Unit): DependencyRepresentation =
+    DependencyBuilder().apply(dependency).build()
+
 
 val threeVessels = brewProcess {
     name = "threeVessels"
-//    tasks = [
-//        task {
-//
-//        }
-//    ]
+    tasks = listOf(
+        mash {},
+        boil { heatingPower = 1000 }
+    )
+    dependencies = listOf(
+        dependency {
+            fromTask = "mash"
+            toTask = "boil"
+            type = DependencyType.STARTS_AFTER_END
+        }
+    )
 }
