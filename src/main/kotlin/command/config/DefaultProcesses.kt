eb7a9e8999ac94129198e5b39cdde2ff170d1550
Jean-Michel Cazaux <jeanmichel.cazaux@gmail.com>
2022-12-25 17:07:31 +0100

    Adding one default brew process

---
+++ src/main/kotlin/command/config/DefaultProcesses.kt
@@ -0,0 +1,58 @@
+package command.config
+
+import brewprocess.DependencyType
+import brewprocess.HeatWater
+import brewprocess.dsl.*
+
+class DefaultProcesses {
+    companion object {
+        val THREE_VESSELS = brewProcess {
+            name = "threeVessels"
+
+            tasks = listOf(
+                heatWater {
+                    name = "Heat Mash Water"
+                    use = HeatWater.For.MASH
+                    heatingPower = 500
+                },
+                mash {},
+                lauter { litersPerMin = 1.0 },
+                boil { heatingPower = 1000 },
+                action {
+                    name = "Sanitize Chiller"
+                    description = "Sanitize Chiller in/with boiling wort"
+                },
+                chill { chillingPower = 500 }
+            )
+
+            dependencies = listOf(
+                dependency {
+                    fromTask = "Heat Mash Water"
+                    toTask = "mash"
+                    type = DependencyType.STARTS_AFTER_END
+                },
+                dependency {
+                    fromTask = "mash"
+                    toTask = "lauter"
+                    type = DependencyType.STARTS_AFTER_END
+                },
+                dependency {
+                    fromTask = "lauter"
+                    toTask = "boil"
+                    type = DependencyType.STARTS_AFTER_END
+                },
+                dependency {
+                    fromTask = "boil"
+                    toTask = "Sanitize Chiller"
+                    type = DependencyType.FINISH_BEFORE_END
+                    delay = 600
+                },
+                dependency {
+                    fromTask = "boil"
+                    toTask = "chill"
+                    type = DependencyType.STARTS_AFTER_END
+                }
+            )
+        }
+    }
+}
