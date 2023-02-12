9b4aabc5229d6a858dde451d5324c7d77a63852a
Jean-Michel Cazaux <jeanmichel.cazaux@ironbird.fr>
2023-02-12 10:05:15 +0100

    Reorganized tests

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -1,14 +1,10 @@
+import TestHelper.Companion.TEST_BDS_DIRECTORY
 import TestHelper.Companion.TEST_EMPTY_DIRECTORY
 import TestHelper.Companion.TEST_HOME_DIRECTORY
-import brewprocess.BrewProcess
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import com.github.stefanbirkner.systemlambda.SystemLambda
 import com.github.stefanbirkner.systemlambda.SystemLambda.restoreSystemProperties
 import com.github.stefanbirkner.systemlambda.SystemLambda.withTextFromSystemIn
 import command.config.ConfigCommand
-import command.config.DefaultProcesses
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
@@ -17,7 +13,6 @@ import org.mockito.Mockito
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.spy
 import picocli.CommandLine
-import kotlin.test.assertEquals
 
 internal class MainKtTest {
 
@@ -60,33 +55,16 @@ internal class MainKtTest {
     }
 
     @Test
-    fun runWithoutAConfigShouldTriggerConfigSetupWhenUserAgreesAndSaveConfig() {
-        val configCommand = ConfigCommand(TEST_EMPTY_DIRECTORY)
-        assertTrue(TEST_EMPTY_DIRECTORY.exists(), "Test empty directory should exists")
-        assertTrue(TEST_EMPTY_DIRECTORY.list().isEmpty(), "Test empty directory should be empty")
+    fun `Run with arguments 'config -s' should trigger config setup`() {
+        val configCommand = ConfigCommand(TEST_BDS_DIRECTORY)
+        val spiedCommand = spy(configCommand)
+        doReturn(CommandLine.ExitCode.OK).`when`(spiedCommand).setUpAndSaveConfig()
 
-        // The below is a hack to generate enough "default" user inputs to go threw the full config set up.
-        val inputs = Array(100) { "" }
-        withTextFromSystemIn(*inputs).execute {
-            BdsApp(arrayOf(configCommand))
+        withTextFromSystemIn("").execute {
+            val app = BdsApp(arrayOf(spiedCommand))
+            app.run(args = arrayOf("config", "-s"))
+            Mockito.verify(spiedCommand).setUpAndSaveConfig()
         }
-
-        // We should have created a config file
-        assertEquals(TEST_EMPTY_DIRECTORY.list().size, 1, "We shall have created one file in the config directory")
-        val configFile = TEST_EMPTY_DIRECTORY.listFiles()[0]
-        assertEquals(configFile.name, "config", "The generated config file shall be named \"config\"")
-        assertTrue(configFile.isFile, "The generated config file shall be a file")
-
-        // The config file shall contain the config for the default process
-        val mapper = jacksonObjectMapper()
-        val config = mapper.readValue<JsonNode>(configFile.readText())
-        val process = mapper.treeToValue(config["process"], BrewProcess::class.java)
-
-        assertEquals(
-            process.name,
-            DefaultProcesses.defaultProcess.name,
-            "The configured process should be the default process"
-        )
     }
 
     @Test
